c1a9b25be4db03f1387dbcc2ab9faa19a8a2061c
Aleksandr Kravtsuk <aleksandr.kravtsuk@gmail.com>
2022-09-14 20:19:36 +0300

    Data type refreshed

--- src/DataType.java
+++ src/DataType.java
@@ -2,15 +2,16 @@
  * Examples of primitive datatypes
  */
 public class DataType {
+    // primitive datatypes
     // Numbers
     int a = 1;
-    long longNumber = 5757858589585;
+    long longNumber = 858589585;
     short shortNumber = -32000;
     float decimalNumber = 2.45f;
     double doubleNumber = 3.456d;
 
     //Character
-    char alpha = "F";
+    char alpha = 'F';
 
     // True False
     boolean hadLunch = true; // had lunch?
@@ -22,4 +23,22 @@ public class DataType {
     float x; // 0.07
     char animal; // ''
     boolean isClassTomorrow; // false
+    //Non/primitive data types
+    String word = " I am developer";
+
+    Character random = 'u';
+
+    Float numberSecond = 2.45F;
+
+    Integer randomNumber; // null
+
+    public static void main(String[] args) {
+        int k = 0;
+        Integer y = null;
+
+
+        System.out.println(k);
+        System.out.println(y);
+    }
+
 }
