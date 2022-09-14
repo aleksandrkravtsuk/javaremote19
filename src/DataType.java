5f1d91af44c9daa08dabb69c6832129c39e02492
Aleksandr Kravtsuk <aleksandr.kravtsuk@gmail.com>
2022-09-14 20:05:27 +0300

    HelloWorld created

---
+++ src/DataType.java
@@ -0,0 +1,25 @@
+/**
+ * Examples of primitive datatypes
+ */
+public class DataType {
+    // Numbers
+    int a = 1;
+    long longNumber = 5757858589585;
+    short shortNumber = -32000;
+    float decimalNumber = 2.45f;
+    double doubleNumber = 3.456d;
+
+    //Character
+    char alpha = "F";
+
+    // True False
+    boolean hadLunch = true; // had lunch?
+    boolean isHoliday = false; //
+    /*
+     * Multi-line comment
+     */
+    int i; // 0
+    float x; // 0.07
+    char animal; // ''
+    boolean isClassTomorrow; // false
+}
