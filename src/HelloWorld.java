d868bf629a9aeea7dcd9667bf095bc287f919ce5
Aleksandr Kravtsuk <aleksandr.kravtsuk@gmail.com>
2022-09-14 19:22:28 +0300

    HelloWorld created

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,11 @@
+/**
+ * The first hello world programm.
+ *
+ * @author Aleksandr Kravtsuk
+ */
+
 public class HelloWorld {
+    public static void main(Strin[] args) {
+        System.out.println("Hello.World!");
+    }
 }
